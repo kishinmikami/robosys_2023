bd18e1bbe7a84ee0fe4b3df3ce5fc9b6fab46627
KM <blackgodstone.k@gmail.com>
2023-10-26 13:15:43 +0900

    update test 4

--- test.bash
+++ test.bash
@@ -18,5 +18,9 @@ out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
