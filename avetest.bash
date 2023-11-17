52ecad79037f2384c21fefecb2df4b3a6c80b69c
KM <blackgodstone.k@gmail.com>
2023-11-17 22:29:25 +0900

    add space test

--- avetest.bash
+++ avetest.bash
@@ -60,5 +60,10 @@ out=$(./ave 1 2 @ 3 4)
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
+###空白のみ###
+out=$(./ave )
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
 exit $res
