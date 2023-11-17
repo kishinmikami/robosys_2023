745cf0df7a4535280ff7268b53ba42981e856e34
KM <blackgodstone.k@gmail.com>
2023-11-17 22:41:44 +0900

    make dispersiontest

---
+++ dispersiontest.bash
@@ -0,0 +1,10 @@
+#!/bin/bash  -xv
+# SPDX-FileCopyrightText: 2023 Kishin Mikami blackgodstone.k@gmail.com
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
